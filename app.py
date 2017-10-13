09d2b93263e0fbc0b1f249bbaf8a6d55d12ac8c0
Gabriel Saruhashi <gabrielsaruhashi@Gabriels-MBP.wireless.yale.internal>
2017-10-13 17:10:14 -0400

    Twilio

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
 #!/usr/bin/env python
 from flask import Flask, flash, redirect, render_template, request, session, \
-abort
+abort, url_for
+#from lxml import html 
+from twilio.rest import Client
 
 app = Flask(__name__)
+
+# Your Account SID from twilio.com/console
+account_sid = "ACf211000f1e2bdf7ad833b9eaf541dc75"
+# Your Auth Token from twilio.com/console
+auth_token  = "5a6ab79b242b9d4fdc8a3ea22d3f4a7a"
+client = Client(account_sid, auth_token)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/sms")
+def sms():
+	
+	message = client.messages.create(
+	    to="+19292849804",
+	    from_="+15005550006",
+	    body="This is the ship that made the Kessel Run in fourteen parsecs?")
+	return "Message Sent!" + message.sid
+
+
+
+'''
+@app.route("scrap")
+def scrap():
+	page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
+	tree = html.fromstring(page.content)
+	#This will create a list of buyers:
+	buyers = tree.xpath('//div[@title="buyer-name"]/text()')
+	#This will create a list of prices
+	prices = tree.xpath('//span[@class="item-price"]/text()')
+	print 'Buyers: ', buyers
+	print 'Prices: ', prices
+
+
+'''
 
 if __name__ == "__main__":
     # Change the host and port name if you want. The default config is port 
