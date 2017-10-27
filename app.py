6babdb10a1482a797f0b8942b95404ddbde8fa27
Henry Townley <htownley@me.com>
2017-10-27 13:48:57 -0400

    Added my own page

--- app.py
+++ app.py
@@ -53,6 +53,14 @@ def scrape():
 
 	f.write('\n')
 	return 'Recipients: '.join(recipients)
+
+
+# Henry's page
+@app.route("/henry")
+def henry():
+	return "Hello, World. This is my page."
+	return render_template('henry.html')
+
 '''
 @app.route("scrap")
 def scrap():
